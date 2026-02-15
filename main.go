09ac6cf380a422ef489e764502bd52c1b12d10e1
Anirudh Saksena <anen08saks@gmail.com>
2026-02-15 20:01:51 +0530

    Keybinds using help module from bubbles

--- main.go
+++ main.go
@@ -4,11 +4,35 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/charmbracelet/bubbles/help"
+	"github.com/charmbracelet/bubbles/key"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
 type model struct {
-	msg string
+	msg  string
+	keys keyMap
+	help help.Model
+}
+
+type keyMap struct {
+	Quit key.Binding
+	New  key.Binding
+	List key.Binding
+	Save key.Binding
+	Back key.Binding
+}
+
+func (k keyMap) ShortHelp() []key.Binding {
+	return []key.Binding{k.New, k.List, k.Save, k.Back, k.Quit}
+}
+
+func (k keyMap) FullHelp() [][]key.Binding {
+	return [][]key.Binding{
+		{k.New, k.List, k.Save},
+		{k.Back, k.Quit},
+	}
 }
 
 func (m model) Init() tea.Cmd {
@@ -18,8 +42,8 @@ func (m model) Init() tea.Cmd {
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c", "q":
+		switch {
+		case key.Matches(msg, m.keys.Quit):
 			return m, tea.Quit
 		}
 	}
@@ -28,12 +52,47 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() string {
-	return m.msg
+	style := lipgloss.NewStyle().
+		Bold(true).
+		Foreground(lipgloss.Color("255")).
+		Background(lipgloss.Color("147")).
+		Width(80).
+		Align(lipgloss.Center)
+
+	welcome := style.Render("Welcome to Note Maker twin :D")
+	helpView := m.help.View(m.keys)
+	view := ""
+
+	return fmt.Sprintf("\n%s\n\n%s\n\n%s", welcome, view, helpView)
 }
 
 func initialModel() model {
+	keys := keyMap{
+		New: key.NewBinding(
+			key.WithKeys("Ctrl+N"),
+			key.WithHelp("Ctrl+N", "Create New File 🗒"),
+		),
+		Quit: key.NewBinding(
+			key.WithKeys("q", "ctrl+c"),
+			key.WithHelp("q", "Quit ⏻"),
+		),
+		List: key.NewBinding(
+			key.WithKeys("ctrl+l"),
+			key.WithHelp("ctrl+l", "list files ☰"),
+		),
+		Save: key.NewBinding(
+			key.WithKeys("ctrl+s"),
+			key.WithHelp("ctrl+s", "save ⎙"),
+		),
+		Back: key.NewBinding(
+			key.WithKeys("esc"),
+			key.WithHelp("esc", "back ➜]"),
+		),
+	}
 	return model{
-		msg: "Welcome to Note Maker twin",
+		msg:  "Welcome to Note Maker twin",
+		keys: keys,
+		help: help.New(),
 	}
 }
 
