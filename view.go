23eaab4f4ff0e77dd8607c4e72c715dc6e9fcfd9
Anirudh Saksena <anen08saks@gmail.com>
2026-03-06 01:16:09 +0530

    View changes : List and viewport preview available on description creation or deletion prompt.

--- view.go
+++ view.go
@@ -80,9 +80,23 @@ func (m model) View() string {
 	header := lipgloss.JoinHorizontal(lipgloss.Center, title, modeStatus, sortStatus)
 
 	if m.deleting {
+		if m.showPreview {
+			var previewView string
+			if m.showingImage {
+				previewView = m.viewport.View()
+			} else {
+				previewView = fmt.Sprintf("%s\n%s\n%s", m.previewHeader(), m.viewport.View(), m.previewFooter())
+			}
+			return fmt.Sprintf(
+				"\n%s\n\n  Are you sure you want to delete this file? (y/n)\n\n%s",
+				header,
+				lipgloss.JoinHorizontal(lipgloss.Top, m.list.View(), "  ", previewView),
+			)
+		}
 		return fmt.Sprintf(
-			"\n%s\n\n  Are you sure you want to delete this file? (y/n)\n",
+			"\n%s\n\n  Are you sure you want to delete this file? (y/n)\n\n%s\n",
 			header,
+			m.list.View(),
 		)
 	}
 
@@ -98,17 +112,18 @@ func (m model) View() string {
 	if m.inputMode {
 		if m.inputStep == 0 {
 			return fmt.Sprintf(
-				"\n%s\n\n%s\n\n%s",
+				"\n%s\n\n  File Name %s\n\n%s",
 				header,
 				m.input.View(),
 				m.list.View(),
 			)
 		}
 		return fmt.Sprintf(
-			"\n%s\n\nFilename: %s\nDescription: %s",
+			"\n%s\n\n%s\n%s\n\n%s",
 			header,
-			m.input.Value(),
+			m.input.View(),
 			m.descInput.View(),
+			m.list.View(),
 		)
 	}
 
